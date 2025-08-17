c52e1b0597cabd69f013aecb5b7e50cc2e5c877c
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-08-17 09:27:43 +0530

    Update main.tf

--- modules/api-gateway/main.tf
+++ modules/api-gateway/main.tf
@@ -1,29 +1,15 @@
 resource "aws_apigatewayv2_api" "http_api" {
   name          = "lambda-http-api"
   protocol_type = "HTTP"
-  tags = var.project_tags
+  tags          = var.project_tags
 }
 
 resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id             = aws_apigatewayv2_api.http_api.id
-  integration_type   = "AWS_PROXY"
-  integration_uri    = var.lambda_integration_uri_arn
-  integration_method = "POST"
-  payload_format_version = "2.0"
-}
-
-resource "aws_apigatewayv2_route" "default_route" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "GET /${var.route_key}"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-  authorization_type = "JWT"
-  authorizer_id      = aws_apigatewayv2_authorizer.cognito_authorizer.id
-}
-
-resource "aws_apigatewayv2_stage" "default_stage" {
-  api_id      = aws_apigatewayv2_api.http_api.id
-  name        = "$default"
-  auto_deploy = true
+  api_id                  = aws_apigatewayv2_api.http_api.id
+  integration_type        = "AWS_PROXY"
+  integration_uri         = var.lambda_integration_uri_arn
+  integration_method      = "POST"
+  payload_format_version  = "2.0"
 }
 
 resource "aws_apigatewayv2_authorizer" "cognito_authorizer" {
@@ -33,7 +19,21 @@ resource "aws_apigatewayv2_authorizer" "cognito_authorizer" {
   identity_sources = ["$request.header.Authorization"]
 
   jwt_configuration {
-    issuer   = "https://cognito-idp.ap-south-1.amazonaws.com/ap-south-1_xTOOFkqlP"
-    audience = ["19st4aqp0ersu7k0rqi5nk8mb"]
+    issuer   = "https://cognito-idp.${var.aws_region}.amazonaws.com/${var.cognito_user_pool_id}"
+    audience = [var.cognito_user_pool_client_id]
   }
+}
+
+resource "aws_apigatewayv2_route" "default_route" {
+  api_id             = aws_apigatewayv2_api.http_api.id
+  route_key          = "GET /${var.route_key}"
+  target             = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+  authorization_type = "JWT"
+  authorizer_id      = aws_apigatewayv2_authorizer.cognito_authorizer.id
+}
+
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
 }
